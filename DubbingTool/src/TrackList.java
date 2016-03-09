4c209c8f0434dfc2123c65916c71b9ad67f2ac8f
courtney <courtneytse20@gmail.com>
2016-03-09 12:45:53 -0500

    Track & TrackList

--- DubbingTool/src/TrackList.java
+++ DubbingTool/src/TrackList.java
@@ -17,19 +17,19 @@ public class TrackList {
 	}
 	
 	public void add(Track newTrack) {
-		
+		tracks.add(newTrack);
 	}
 	
 	public Track get(int index) {
-		
+		tracks.get(index);
 	}
 	
 	public Track remove(int index) {
-		
+		track.remove(index);
 	}
 	
 	public int numTracks() {
-		
+		return tracks.size();
 	}
 	
 	public List<Track> failedTracks() {
