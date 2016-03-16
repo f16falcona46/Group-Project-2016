83fe97d43ccf4556aff67ee171ad7e129dbee863
Lord-Zorak <rssorak@gmail.com>
2016-03-16 12:57:38 -0400

    no

--- DubbingTool/src/PicturePaneTester.java
+++ DubbingTool/src/PicturePaneTester.java
@@ -1,6 +1,9 @@
 
 public class PicturePaneTester {
 	TrackList list = new TrackList();
-	Track track = new Track(1,list);
+	Track track0 = new Track(1,list);
+	Track track1 = new Track("name", 2.5,5,1,);
+	Track track2 = new Track(1,list);
 	PicturePane pane = new PicturePane(list);
+	
 }
