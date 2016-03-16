fdbe25ca467143257113e2b4e929aa4e42bc67ff
Jason Li <jcl01010011@gmail.com>
2016-03-16 12:41:15 -0400

    Incorrect serial UID for BadFileException fixed

--- DubbingTool/src/BadFileException.java
+++ DubbingTool/src/BadFileException.java
@@ -1,3 +1,3 @@
 public class BadFileException extends Exception {
-	private static final long serialVersionUID = 3102964114337523385L;
+	private static final long serialVersionUID = -3098962438753984434L;
 }
